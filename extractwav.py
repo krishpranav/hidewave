f82412d3411bbc386c4e2f959adb3c2c07ace483
krishpranav <krisna.pranav@gmail.com>
2021-06-30 19:34:37 +0530

    [+] extract msg [+]

--- extractwav.py
+++ extractwav.py
@@ -22,3 +22,20 @@ def help():
 optional arguments:
   -h, --help    show this help message and exit
   -f AUDIOFILE  Select Audio File''')
+
+
+def banner():
+    print("HIDE-MSG")
+
+def ex_msg(af):
+    if not arged:
+      help()
+    else:
+        print ("Please wait...")
+        waveaudio = wave.open(af, mode='rb')
+        frame_bytes = bytearray(list(waveaudio.readframes(waveaudio.getnframes())))
+        extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
+        string = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
+        msg = string.split("###")[0]
+        print("Your Secret Message is: \033[1;91m"+msg+"\033[0m")
+        waveaudio.close()
